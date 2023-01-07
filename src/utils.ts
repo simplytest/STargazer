3852e72d0fc9680060d88c99a3f7b0a02abab678
Curve <fynnbwdt@gmail.com>
2023-01-07 20:49:58 +0100

    feat(utils): expand getInspectedElement, add "getParentOfInspected"

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,43 @@
-async function getInspectedElement(): Promise<string> {
-    return new Promise<string>((resolve) => {
-        chrome.devtools.inspectedWindow.eval(`$0.outerHTML`, {}, (result) => {
-            resolve(result as unknown as string);
+interface InspectedElement {
+    html: string;
+    node?: Element;
+    innerText: string;
+};
+
+async function getInspectedElement(): Promise<InspectedElement> {
+    const element = await new Promise<InspectedElement>((resolve) => {
+        chrome.devtools.inspectedWindow.eval(`[$0.outerHTML, $0.innerText]`, {}, (result) => {
+            resolve({ html: result[0], innerText: result[1] })
         });
     });
+
+    const parser = new DOMParser();
+    element.node = parser.parseFromString(element.html, "text/html").body.firstElementChild;
+
+    return element;
 }
 
-async function getDom(): Promise<string> {
-    return new Promise<string>((resolve) => {
+async function getParentOfInspected(): Promise<InspectedElement> {
+    const element = await new Promise<InspectedElement>((resolve) => {
+        chrome.devtools.inspectedWindow.eval(`[$0.parentElement.outerHTML, $0.parentElement.innerText]`, {}, (result) => {
+            resolve({ html: result[0], innerText: result[1] })
+        });
+    });
+
+    const parser = new DOMParser();
+    element.node = parser.parseFromString(element.html, "text/html").body.firstElementChild;
+
+    return element;
+}
+
+async function getDom(): Promise<Document> {
+    const html = await new Promise<string>((resolve) => {
         chrome.devtools.inspectedWindow.eval(`document.body.outerHTML`, {}, (result) => {
             resolve(result as unknown as string);
         });
     });
+
+    return new DOMParser().parseFromString(html, "text/html");
 }
 
 async function getHotkey(): Promise<string> {
@@ -31,9 +57,12 @@ function invokeDebugger() {
 }
 
 export {
+    getParentOfInspected,
     getInspectedElement,
     invokeDebugger,
     getVersion,
     getHotkey,
     getDom,
+
+    InspectedElement
 };
