355aa06ae5158e5a5ee3a15063668bc37e70c4f9
Curve <fynnbwdt@gmail.com>
2023-01-12 13:43:05 +0100

    fix(css): prevent generation of invalid selectors, correct index

--- src/selectors/css.ts
+++ src/selectors/css.ts
@@ -12,7 +12,7 @@ function constructConditions(select: Selector) {
   }
 
   if ('index' in select) {
-    selector += `:nth-child(${select.index})`;
+    selector += `:nth-child(${select.index + 1})`;
   }
 
   return selector;
@@ -22,9 +22,11 @@ function generateCSS(chain: SelectorChain): string {
   let selector = '';
 
   // ? CSS Selectors can't check for text
-  const valid = chain.filter(x => !('text' in x));
+  if (chain.indexOf(chain.find(x => 'text' in x)) !== -1) {
+    return selector;
+  }
 
-  for (const [index, select] of valid.entries()) {
+  for (const [index, select] of chain.entries()) {
     if ('amend' in select) {
       selector += constructConditions(select);
       continue;
