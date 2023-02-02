2d9260c4a9aa7bfd3ff530ded3dfd14417a9945e
Curve <fynnbwdt@gmail.com>
2023-02-02 19:29:27 +0100

    refactor: primary color to orange

--- pages/theme.ts
+++ pages/theme.ts
@@ -3,7 +3,7 @@ import { MantineThemeOverride } from '@mantine/core';
 const theme: MantineThemeOverride = {
   loader: 'dots',
   colorScheme: 'dark',
-  primaryColor: 'yellow',
+  primaryColor: 'orange',
 };
 
 export default theme;
