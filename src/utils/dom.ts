f8c438664fd17de3812a220b00d0a7e132f51c07
Curve <fynnbwdt@gmail.com>
2023-02-03 14:02:32 +0100

    refactor(dom): cleanup duplicate code

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -4,116 +4,87 @@ import { execute, inject } from './chrome';
 
 const canUseDevTools = () => !!chrome.devtools?.inspectedWindow;
 
-async function getDom(): Promise<Document> {
-  const html = canUseDevTools()
-    ? await execute<string>('document.body.outerHTML')
-    : await inject(() => document.body.outerHTML);
+async function exec<R, T extends unknown[]>(func: (...args: T) => R, ...args: T) {
+  if (!canUseDevTools()) {
+    return inject<R, T>(func, ...args);
+  }
 
+  const serializedFunc = func.toString().replaceAll(/window\[.indiana_inspected.\]/gi, '$0');
+  return execute<R>(`(${serializedFunc})(${args})`);
+}
+
+async function getDom(): Promise<Document> {
+  const html = await exec(() => document.body.outerHTML);
   return getFromHTML(html, 'Document');
 }
 
 async function getInspected(): Promise<Inspected> {
-  const [html, innerText] = canUseDevTools()
-    ? await execute<[string, string]>('[$0.outerHTML, $0.innerText]')
-    : await inject<[string, string], []>(() => [
-        window['indiana_inspected_element'].outerHTML,
-        window['indiana_inspected_element'].innerText,
-      ]);
+  const [html, innerText] = await exec(() => {
+    const element: HTMLElement = window['indiana_inspected'];
+    return [element.outerHTML, element.innerText];
+  });
 
   const element = getFromHTML(html, 'Element');
   return { element, html, innerText };
 }
 
 async function getInspectedParent(): Promise<[Inspected, number]> {
-  const [html, innerText] = canUseDevTools()
-    ? await execute<[string, string]>('[$0.parentElement.outerHTML, $0.parentElement.innerText]')
-    : await inject<[string, string], []>(() => [
-        window['indiana_inspected_element'].parentElement.outerHTML,
-        window['indiana_inspected_element'].parentElement.innerText,
-      ]);
+  const [html, innerText] = await exec(() => {
+    const element: HTMLElement = window['indiana_inspected'];
+    return [element.parentElement.outerHTML, element.parentElement.innerText];
+  });
 
-  const index = canUseDevTools()
-    ? await execute<number>('[...$0.parentElement.children].indexOf($0)')
-    : await inject<number, []>(() =>
-        [...window['indiana_inspected_element'].parentElement.children].indexOf(window['indiana_inspected_element'])
-      );
+  const index = await exec(() => {
+    const element: HTMLElement = window['indiana_inspected'];
+    return [...(element.parentElement.children as unknown as HTMLElement[])].indexOf(element);
+  });
 
   const element = getFromHTML(html, 'Element');
   return [{ element, html, innerText }, index];
 }
 
 async function getInspectedParentCount(): Promise<number> {
-  if (!canUseDevTools()) {
-    return await inject(() => {
-      const parent = (x, n, i = 0) => {
-        if (i >= n) {
-          return x;
-        } else {
-          return parent(x.parentElement, n, i + 1);
-        }
-      };
+  return await exec(() => {
+    const element: HTMLElement = window['indiana_inspected'];
 
-      let parents = 0;
-
-      for (let i = 0; parent(window['indiana_inspected_element'], i); i++) {
-        parents++;
-      }
-
-      return parents;
-    });
-  }
-
-  return await execute<number>(`
-    parent = (x, n, i = 0) => { 
+    const parent = (x: HTMLElement, n: number, i = 0) => {
       if (i >= n) {
         return x;
       } else {
         return parent(x.parentElement, n, i + 1);
-      } 
+      }
     };
 
-    parents = 0;
+    let parents = 0;
 
-    for (let i = 0; parent($0, i); i++)
-    {
+    for (let i = 0; parent(element, i); i++) {
       parents++;
     }
 
-    parents;
-`);
+    return parents;
+  });
 }
 
 async function getInspectedParentRecursive(n: number): Promise<[Inspected, number]> {
-  const [html, innerText, index] = canUseDevTools()
-    ? await execute<[string, string, number]>(`
-    parent = (x, n, i = 0) => { 
+  const [html, innerText, index] = await exec(n => {
+    const element: HTMLElement = window['indiana_inspected'];
+
+    const parent = (x: HTMLElement, n: number, i = 0) => {
       if (i >= n) {
         return x;
       } else {
         return parent(x.parentElement, n, i + 1);
-      } 
+      }
     };
 
-    indiana_rtn = parent($0, ${n});
-    [indiana_rtn.outerHTML, indiana_rtn.innerText, [...indiana_rtn.parentElement.children].filter(x => x.tagName === indiana_rtn.tagName).indexOf(indiana_rtn)]
-  `)
-    : await inject<[string, string, number], [number]>(n => {
-        const parent = (x, n, i = 0) => {
-          if (i >= n) {
-            return x;
-          } else {
-            return parent(x.parentElement, n, i + 1);
-          }
-        };
+    const rtn = parent(element, n);
 
-        const indiana_rtn = parent(window['indiana_inspected_element'], n);
-
-        return [
-          indiana_rtn.outerHTML,
-          indiana_rtn.innerText,
-          [...indiana_rtn.parentElement.children].filter(x => x.tagName === indiana_rtn.tagName).indexOf(indiana_rtn),
-        ];
-      }, n);
+    return [
+      rtn.outerHTML,
+      rtn.innerText,
+      [...rtn.parentElement.children].filter(x => x.tagName === rtn.tagName).indexOf(rtn),
+    ];
+  }, n);
 
   const element = getFromHTML(html, 'Element');
   return [{ element, html, innerText }, index];
