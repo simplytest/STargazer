4b0cf889df1343683be3d93ad44d0905444625d0
Curve <fynnbwdt@gmail.com>
2023-02-15 19:53:50 +0100

    fix: highlight removal

--- pages/devtools/bootstrap.ts
+++ pages/devtools/bootstrap.ts
@@ -1,4 +1,7 @@
+import { removeHighlights } from '../../src/highlight';
+
 chrome.devtools.panels.elements.createSidebarPane('STargazer', sidebar => {
-  chrome.runtime.connect();
+  const port = chrome.runtime.connect();
   sidebar.setPage('/pages/devtools/index.html');
+  port.onDisconnect.addListener(() => removeHighlights());
 });
