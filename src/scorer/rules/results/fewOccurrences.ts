eefad03a25ecfd49db136dfa23ba69e99b0c2229
Curve <fynnbwdt@gmail.com>
2023-03-17 11:30:14 +0100

    refactor: apply onlyUnique in "fewOccurances" rule
    This fixes a bug where no selectors would be shown if "Hide ambiguous" was on and the best selectors had a above average score and too weak of a penalty for their non-unique occurrences, this fixes this problem by making them unacceptable at scoring stage, which in turn causes the deepParent generator to be activated

--- src/scorer/rules/results/fewOccurrences.ts
+++ src/scorer/rules/results/fewOccurrences.ts
@@ -1,11 +1,16 @@
 import { Result } from '../../../types/generator';
+import { Settings } from '../../../types/settings';
 import scores from '../../scores';
 
-export default function (result: Result) {
+export default function (result: Result, { onlyUnique }: Settings) {
   const { occurrences } = result;
 
   if (occurrences === 1) {
     return scores.desired;
+  }
+
+  if (onlyUnique && occurrences != 1) {
+    return scores.unacceptable;
   }
 
   return scores.awful;
