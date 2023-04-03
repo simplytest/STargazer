507a2a3df9ce6069b461fd37866580c4f9f5879e
Curve <fynnbwdt@gmail.com>
2023-04-03 17:14:53 +0200

    refactor(standalone-tag): exclude "body"

--- src/scorer/rules/results/noStandaloneTag.ts
+++ src/scorer/rules/results/noStandaloneTag.ts
@@ -1,11 +1,12 @@
 import { Result } from '../../../types/generator';
+import { ByTag } from '../../../types/selector';
 import scores from '../../scores';
 
 export default function (result: Result) {
   const { chain } = result;
-  const standaloneTags = chain.filter(x => Object.keys(x).length === 1 && 'tag' in x);
+  const standaloneTags = chain.filter(x => Object.keys(x).length === 1 && 'tag' in x) as ByTag[];
 
-  if (standaloneTags.length === chain.length) {
+  if (standaloneTags.length === chain.length && standaloneTags.at(0).tag != 'body') {
     return scores.unacceptable;
   }
 
