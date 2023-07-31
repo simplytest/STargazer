0b1e683fc14ce4807ec84eab789399af97b21e99
Curve <fynnbwdt@gmail.com>
2023-07-31 19:28:00 +0200

    feat(picker): add passthrough mode
    Also fixes issues with fixed elements on page

--- src/client/picker.tsx
+++ src/client/picker.tsx
@@ -1,15 +1,18 @@
 import theme from "../../shared/theme";
 import { scripting } from "../extension/scripting";
+import { INSPECTED_ID, MAXIMUM_ZINDEX } from "./constants";
 
 type color_t = string;
 type style_t = { background: color_t };
 
 export class picker
 {
-    static readonly MAXIMUM_ZINDEX = "2147483647";
+    static readonly SIGNAL_ID = "stargazer_abortsignal";
+    static readonly ALERT_ID = "stargazer_alert";
     static readonly ID = "stargazer_picker";
-
+    
     private overlay: HTMLDivElement;
+    private last_event: MouseEvent;
 
     static async start() 
     {
@@ -35,26 +38,76 @@ export class picker
         });
     }
 
+    private static is_sticky(element: Element)
+    {
+        if (!element || element === document.body)
+        {
+            return false;
+        }
+
+        const style = element.computedStyleMap();
+        const position = style.get("position").toString();
+
+        if (position.match(/fixed|sticky/i))
+        {
+            return true;
+        }
+
+        return picker.is_sticky(element.parentElement);
+    }
+
+    private static alert(message: string)
+    {
+        const root = document.createElement("div");
+
+        root.id = picker.ALERT_ID;
+        root.style.position = "fixed";
+        root.style.borderRadius = "15px";
+        root.style.zIndex = MAXIMUM_ZINDEX;
+        root.style.transition = "all 300ms ease";
+        root.style.transform = "translate(calc(50vw - 50%), calc(50vh - 50%))";
+
+        root.style.padding = "10px";
+        root.style.background = "#FFFFFF";
+        root.style.border = "1px solid #000000";
+
+        root.style.display = "flex";
+        root.style.alignItems = "center";
+        root.style.justifyContent = "center";
+
+        const text = document.createElement("div");
+        text.innerHTML = message;
+
+        root.appendChild(text);
+        const alert = document.body.appendChild(root);
+
+        setTimeout(() => 
+        {
+            alert.remove();
+        }, 5000);
+    }
+
     private create_overlay(style: style_t)
     {
         const overlay  = document.createElement("div");
         
         overlay.id = picker.ID;
         
+        overlay.style.cursor = "crosshair";
         overlay.style.position = "absolute";
         overlay.style.transition = "all 300ms ease";
 
         overlay.style.textAlign = "center";
         overlay.style.verticalAlign = "middle";
         
-        overlay.style.zIndex = picker.MAXIMUM_ZINDEX;
+        overlay.style.zIndex = MAXIMUM_ZINDEX;
         overlay.style.backgroundColor = style.background;
 
         return overlay;
     }
 
     private mouse_move(event: MouseEvent)
-    {
+    {        
         let target = event.target as Element;
         const over_self = target?.id === picker.ID;
 
@@ -66,6 +119,7 @@ export class picker
             return;
         }
 
+        const style = this.overlay.style;
         const dim = target.getBoundingClientRect();
 
         this.overlay.textContent = target.tagName.toLowerCase();
@@ -75,12 +129,89 @@ export class picker
             this.overlay.textContent += ` #${target.id}`;
         }
 
-        this.overlay.style.width = `${dim.width}px`;
-        this.overlay.style.height = `${dim.height}px`;
-        this.overlay.style.lineHeight = `${dim.height}px`;
+        style.width = `${dim.width}px`;
+        style.height = `${dim.height}px`;
+        style.lineHeight = `${dim.height}px`;
 
-        this.overlay.style.top = `${dim.top + window.scrollY}px`;
-        this.overlay.style.left = `${dim.left + window.scrollX}px`;
+        let { top, left } = dim;
+
+        if (picker.is_sticky(target))
+        {
+            style.position = "fixed";
+        }
+        else
+        {
+            top += window.scrollY; 
+            left += window.scrollX; 
+            style.position = "absolute";
+        }
+
+        style.top = `${top}px`;
+        style.left = `${left}px`;
+
+        this.last_event = event;
+    }
+
+    private mouse_down(event: MouseEvent)
+    {
+        const under_mouse = document.elementsFromPoint(event.clientX, event.clientY);
+        let target: Element = null;
+
+        if (under_mouse[0]?.id === picker.ID)
+        {
+            target = under_mouse[1];
+        }
+        else
+        {
+            target = under_mouse[0];
+        }
+
+        if (!target)
+        {
+            return;
+        }
+
+        window[INSPECTED_ID] = target;
+        picker.destroy();
+    }
+
+    private key_down(event: KeyboardEvent)
+    {
+        /*
+            This is a small convenience feature.
+
+            When you press 'Shift' while picking an element,
+            all Pointer-Events will be passed through to the 
+            underlying element - thus allowing you to fully 
+            interact with them.
+            However this will not allow you to simply click
+            on the element to pick it, this will then be
+            possible by pressing 'P'.
+        */
+
+        const { style } = this.overlay;
+        const key = event.key.toLowerCase();
+        const passthrough = this.overlay.style.pointerEvents === "none";
+
+        if (key === "shift")
+        {
+            if (!passthrough)
+            {
+                const message = 
+                `You have enabled Passthrough-Picking.<br>
+                You can toggle this feature by pressing [Shift].<br>
+                To pick an element while this mode is activated, simply press [P]`;
+
+                picker.alert(message);
+            }
+
+            style.pointerEvents = passthrough ? "all" : "none";
+        }
+
+        if (key === "p" && passthrough)
+        {
+            this.mouse_down(new MouseEvent("mousedown", this.last_event));
+        }
     }
 
     private create(style: style_t)
@@ -94,18 +225,26 @@ export class picker
         const overlay = this.create_overlay(style);
         this.overlay = document.body.appendChild(overlay);
 
-        document.addEventListener("mousemove", e => this.mouse_move(e));
+        this.overlay.addEventListener("mousedown", e => this.mouse_down(e));
+
+        window[picker.SIGNAL_ID] = new AbortController();
+        document.addEventListener("keydown", e => this.key_down(e), { signal: window[picker.SIGNAL_ID].signal });
+        document.addEventListener("mousemove", e => this.mouse_move(e), { signal: window[picker.SIGNAL_ID].signal });
     }
 
     private static destroy()
     {
         const instance = document.getElementById(picker.ID);
+        const abort_signal = window[picker.SIGNAL_ID];
 
-        if (!instance) 
+        if (instance) 
         {
-            return;
+            instance.remove();
         }
 
-        instance.remove();
+        if (abort_signal)
+        {
+            abort_signal.abort();
+        }
     }
 }
