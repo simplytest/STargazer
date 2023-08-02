6370c766abfac08524ca80b125cc6314989490a0
Curve <fynnbwdt@gmail.com>
2023-08-02 16:38:47 +0200

    feat(sidebar): add transitions

--- pages/sidebar/index.tsx
+++ pages/sidebar/index.tsx
@@ -1,32 +1,39 @@
-import { Stack, Tabs, useMantineTheme } from "@mantine/core";
+import { Stack, Tabs, Transition, useMantineTheme } from "@mantine/core";
 import { IconBrain, IconSettings } from "@tabler/icons-react";
 import Footer from "../../components/footer";
 import Generator from "../../components/generator";
 import Settings from "../../components/settings";
 import create_root from "../../src/react";
+import { useState } from "react";
 
 function SideBar()
 {
     const theme = useMantineTheme();
     const background = theme.colors[theme.colorScheme === "dark" ? "dark" : "gray"][4];
 
+    const [tab, set_tab] = useState("generator");
+
     return <Stack style={{ height: "100%" }}>
-        <Tabs defaultValue="generator">
+        <Tabs value={tab} onTabChange={set_tab}>
             <Tabs.List grow bg={background}>
-                <Tabs.Tab value="generator" icon={<IconBrain/>}>Generator</Tabs.Tab>
-                <Tabs.Tab value="settings" icon={<IconSettings/>}>Configure</Tabs.Tab>
+                <Tabs.Tab value="generator" icon={<IconBrain />}>Generator</Tabs.Tab>
+                <Tabs.Tab value="settings" icon={<IconSettings />}>Configure</Tabs.Tab>
             </Tabs.List>
 
             <Tabs.Panel value="generator">
-                <Generator />
+                <Transition transition="fade" mounted={tab === "generator"}>
+                    {style => <Generator style={style} />}
+                </Transition>
             </Tabs.Panel>
-            
+
             <Tabs.Panel value="settings">
-                <Settings />
+                <Transition transition="fade" mounted={tab === "settings"}>
+                    {style => <Settings style={style} />}
+                </Transition>
             </Tabs.Panel>
         </Tabs>
-        <Footer/>
+        <Footer />
     </Stack>;
 }
 
-create_root(<SideBar/>);
+create_root(<SideBar />);
