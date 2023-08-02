b3038ce2a544749a1d2dc60c8fbe497fd7c11333
Curve <fynnbwdt@gmail.com>
2023-08-02 16:36:50 +0200

    refactor(footer): make sticky

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,13 @@
-import { Divider, Group, Image, Stack, Text } from "@mantine/core";
-import React from "react";
+import { Group, Image, Paper, Stack, Text } from "@mantine/core";
 
 export default function Footer()
 {
-    return <Stack align="center" style={{ marginTop: "auto" }} mb={15}>
-        <Divider style={{ width: "80%" }} my="sm"/>
-        <Group noWrap>
-            <Image src="/assets/logo.png" height={32} width={32} radius="md" />
-            <Text size="sm">Copyright © {new Date().getFullYear()} <a href="https://simplytest.de/" target="_blank" rel="noreferrer" >SimplyTest</a>. All Rights Reserved</Text>
-        </Group>
+    return <Stack align="center" style={{ position: "fixed", width: "100%", bottom: 0 }}>
+        <Paper style={{ width: "100%" }} shadow="0px -11px 33px -10px rgba(0,0,0,0.15)">
+            <Group m="md" noWrap>
+                <Image src="/assets/logo.png" height={32} width={32} radius="sm" />
+                <Text size="sm">Copyright © {new Date().getFullYear()} <a href="https://simplytest.de/" target="_blank" rel="noreferrer">SimplyTest</a>. All Rights Reserved</Text>
+            </Group>
+        </Paper>
     </Stack>;
 }
