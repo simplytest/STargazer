8095580e40fe4f7383449921614e5c3bbdf7f9df
Curve <fynnbwdt@gmail.com>
2023-08-02 16:38:30 +0200

    feat(settings): add generator settings and build info

--- components/settings.tsx
+++ components/settings.tsx
@@ -1,11 +1,65 @@
-import { Stack, Switch, useMantineColorScheme } from "@mantine/core";
+import { Box, Center, Divider, Group, NumberInput, SegmentedControl, Stack, Switch, Text, useMantineColorScheme } from "@mantine/core";
+import { IconBrain, IconInfoCircle, IconMoon, IconPalette, IconSun, TablerIconsProps } from "@tabler/icons-react";
+import { CSSProperties, Fragment, ReactNode } from "react";
+import useStorage from "../src/hooks/storage";
+import { meta } from "../src/extension/meta";
 
-export default function Settings()
+function IconText({ Icon, text, center }: {Icon: (props: TablerIconsProps) => JSX.Element, text: string, center?: boolean})
 {
-    const { colorScheme: theme, toggleColorScheme: toggle_theme } = useMantineColorScheme();
-    const dark = theme === "dark";
+    const Wrapping = center ? Center : Fragment;
 
-    return <Stack m="lg">
-        <Switch checked={dark} onChange={() => toggle_theme()} label="Dark Theme" />
+    return <Wrapping>
+        <Icon size={12} />
+        <Box ml={5}>{text}</Box>
+    </Wrapping>;
+}
+
+function Section({ Icon, text }: {Icon: (props: TablerIconsProps) => JSX.Element, text: string})
+{
+    return <Divider label={<IconText Icon={Icon} text={text} />} />;
+}
+
+type OptionProps<P extends object> = Omit<P, "w"> &
+{
+    "no-fixed"?: boolean;
+    "option-label": string;
+    Control: (props: P) => ReactNode;
+}
+
+function Option<P extends object>({ "option-label": option_label, Control, "no-fixed": no_fixed, ...data }: OptionProps<P>)
+{
+    return <Group position="apart" noWrap>
+        <Text>{option_label}</Text>
+        <Control {...data as P} {...(no_fixed ? {} : { w: 150 })} />
+    </Group>;
+}
+
+
+export default function Settings({ style }: {style?: CSSProperties})
+{
+    const { colorScheme: theme, toggleColorScheme: set_theme } = useMantineColorScheme();
+
+    const [hide, set_hide] = useStorage("hide-ambiguous", true);
+    const [type, set_type] = useStorage("selector-type", "xpath");
+    const [to_show, set_to_show] = useStorage("result-to-show", 3);
+
+    return <Stack m="lg" style={style}>
+        <Section text="Appearance" Icon={IconPalette} />
+        <Option Control={SegmentedControl} option-label="Theme" data={[
+            { label: <IconText Icon={IconSun} text="Light" center />, value: "light" },
+            { label: <IconText Icon={IconMoon} text="Dark" center />, value: "dark" },
+        ]} value={theme} onChange={set_theme} />
+
+        <Section text="Generator" Icon={IconBrain} />
+        <Option Control={SegmentedControl} option-label="Selector Type" data={[
+            { label: "CSS", value: "css" },
+            { label: "XPath", value: "xpath" },
+        ]} value={type} onChange={set_type} />
+        <Option Control={NumberInput} option-label="Results to Show" min={1} precision={0} stepHoldDelay={500} stepHoldInterval={100} variant="filled" value={to_show} onChange={set_to_show} />
+        <Option Control={Switch} option-label="Hide Ambiguous" checked={hide} onChange={e => set_hide(e.currentTarget.checked)} no-fixed />
+
+        <Section text="Information" Icon={IconInfoCircle} />
+        <Option Control={Text<"div">} option-label="Version" align="right">{meta.version()}</Option>
+        <Option Control={Text<"div">} option-label="Build Date" align="right">{meta.build()}</Option>
     </Stack>;
 }
