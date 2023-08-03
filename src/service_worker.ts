7b2889d13ddeaae96ed86b77d0071dfe362861c3
Curve <fynnbwdt@gmail.com>
2023-08-03 22:00:36 +0200

    feat(service-worker): handle "request_score"

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,6 +1,9 @@
 import { picker } from "./client/picker";
 import { sidebar } from "./client/sidebar";
 import { commands } from "./extension/commands";
+import { messages } from "./extension/messages";
+import { request_score } from "./generator/messages";
+import { score } from "./generator/scorer";
 
 commands.register("start-picking", async () =>
 {
@@ -15,4 +18,14 @@ commands.register("open-sidebar", async () =>
 chrome.action.onClicked.addListener(async () =>
 {
     await sidebar.open();
+});
+
+/*
+# We handle selector scoring in the background script because it allows us
+# to reduce the loading time of the sidebar.
+*/
+
+messages.register(request_score, msg =>
+{
+    return msg.chains.map(x => ({ score: score(x), chain: x }));
 });
