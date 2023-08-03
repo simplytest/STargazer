7b0c8db2f9236a26f35a489ab3069a3e7ef3d9a5
Curve <fynnbwdt@gmail.com>
2023-08-03 21:59:29 +0200

    feat(settings): add developer settings, update selectors on type change

--- components/settings.tsx
+++ components/settings.tsx
@@ -1,8 +1,9 @@
 import { Box, Center, Divider, Group, NumberInput, SegmentedControl, Stack, Switch, Text, useMantineColorScheme } from "@mantine/core";
-import { IconBrain, IconInfoCircle, IconMoon, IconPalette, IconSun, TablerIconsProps } from "@tabler/icons-react";
+import { IconBrain, IconBug, IconInfoCircle, IconMoon, IconPalette, IconSun, TablerIconsProps } from "@tabler/icons-react";
 import { CSSProperties, Fragment, ReactNode } from "react";
-import useStorage from "../src/hooks/storage";
 import { meta } from "../src/extension/meta";
+import useStorage from "../src/hooks/storage";
+import { worker } from "../src/client/worker";
 
 function IconText({ Icon, text, center }: {Icon: (props: TablerIconsProps) => JSX.Element, text: string, center?: boolean})
 {
@@ -39,9 +40,17 @@ export default function Settings({ style }: {style?: CSSProperties})
 {
     const { colorScheme: theme, toggleColorScheme: set_theme } = useMantineColorScheme();
 
-    const [hide, set_hide] = useStorage("hide-ambiguous", true);
     const [type, set_type] = useStorage("selector-type", "xpath");
     const [to_show, set_to_show] = useStorage("result-to-show", 3);
+
+    const [show_raw, set_show_raw] = useStorage("show-raw", false);
+    const [show_scores, set_show_scores] = useStorage("show-scores", false);
+
+    const change_type = (value: string) =>
+    {
+        set_type(value);
+        worker.update_selectors();
+    };
 
     return <Stack m="lg" style={style}>
         <Section text="Appearance" Icon={IconPalette} />
@@ -54,12 +63,19 @@ export default function Settings({ style }: {style?: CSSProperties})
         <Option Control={SegmentedControl} option-label="Selector Type" data={[
             { label: "CSS", value: "css" },
             { label: "XPath", value: "xpath" },
-        ]} value={type} onChange={set_type} />
+        ]} value={type} onChange={change_type} />
         <Option Control={NumberInput} option-label="Results to Show" min={1} precision={0} stepHoldDelay={500} stepHoldInterval={100} variant="filled" value={to_show} onChange={set_to_show} />
-        <Option Control={Switch} option-label="Hide Ambiguous" checked={hide} onChange={e => set_hide(e.currentTarget.checked)} no-fixed />
 
         <Section text="Information" Icon={IconInfoCircle} />
         <Option Control={Text<"div">} option-label="Version" align="right">{meta.version()}</Option>
         <Option Control={Text<"div">} option-label="Build Date" align="right">{meta.build()}</Option>
+
+        {meta.developer() &&
+        <>
+            <Section text="Developer Options" Icon={IconBug} />
+            <Option Control={Switch} option-label="Show Raw" no-fixed checked={show_raw} onChange={e => set_show_raw(e.currentTarget.checked)} />
+            <Option Control={Switch} option-label="Show Scores" no-fixed checked={show_scores} onChange={e => set_show_scores(e.currentTarget.checked)} />
+        </>
+        }
     </Stack>;
 }
