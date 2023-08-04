bcb42ab1dc2ed26221e8852230ac90a7c8b4b480
Curve <fynnbwdt@gmail.com>
2023-08-04 12:58:22 +0200

    feat: add highlighter

--- src/client/export.ts
+++ src/client/export.ts
@@ -1,7 +1,9 @@
+import { highlighter } from "./highlight";
 import { picker } from "./picker";
 import { sidebar } from "./sidebar";
 import { worker } from "./worker";
 
+global.highlighter = highlighter;
 global.sidebar = sidebar;
 global.picker = picker;
 global.worker = worker;
---
+++ src/client/highlight.ts
@@ -0,0 +1,115 @@
+import theme from "../../shared/theme";
+import { scripting } from "../extension/scripting";
+import { MAXIMUM_ZINDEX } from "./constants";
+import { picker } from "./picker";
+import { sidebar } from "./sidebar";
+
+type color_t = string;
+type style_t = { background: color_t };
+
+export class highlighter
+{
+    static readonly ID = "stargazer_highlight";
+
+    static async start(selector: string)
+    {
+        await scripting.export();
+
+        const style: style_t = {
+            background: `${theme.colors.orange[0]}80`,
+        };
+
+        await scripting.execute((style, selector) =>
+        {
+            const instance = new highlighter();
+            instance.create(style, selector);
+        },
+        style, selector);
+    }
+
+    static async stop()
+    {
+        scripting.execute(() =>
+        {
+            highlighter.destroy();
+        });
+    }
+
+    private create_overlay(style: style_t)
+    {
+        const overlay = document.createElement("div");
+        overlay.setAttribute("name", highlighter.ID);
+
+        overlay.style.position = "absolute";
+        overlay.style.zIndex = MAXIMUM_ZINDEX;
+        overlay.style.backgroundColor = style.background;
+
+        return overlay;
+    }
+
+    private populate(selector: string, elements: Element[])
+    {
+        if (selector.startsWith("//"))
+        {
+            const results = document.evaluate(selector, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+            for (let i = 0; results.snapshotLength > i; i++)
+            {
+                elements.push(results.snapshotItem(i) as Element);
+            }
+
+            return;
+        }
+
+        [...document.querySelectorAll(selector)].forEach(x => elements.push(x));
+    }
+
+    private create(style: style_t, selector: string)
+    {
+        // We first delete the old instance
+
+        highlighter.destroy();
+
+        // Then we setup the new instance(s)
+
+        const targets: Element[] = [];
+
+        try
+        {
+            this.populate(selector, targets);
+        }
+        catch (e)
+        {
+            console.debug("[STargazer] Encountered invalid selector while highlighting");
+        }
+
+        const sidebar_instance = document.getElementById(sidebar.ID);
+
+        const append = (element) =>
+        {
+            if (sidebar_instance)
+            {
+                return document.body.insertBefore(element, sidebar_instance);
+            }
+
+            return document.body.appendChild(element);
+        };
+
+        for (let i = 0; Math.min(10, targets.length) > i; i++)
+        {
+            const overlay = this.create_overlay(style);
+            picker.position_over(append(overlay), targets[i]);
+        }
+    }
+
+    public static destroy()
+    {
+        let instances = document.getElementsByName(highlighter.ID);
+
+        while (instances && instances.length > 0)
+        {
+            instances[0].remove();
+            instances = document.getElementsByName(highlighter.ID);
+        }
+    }
+}
