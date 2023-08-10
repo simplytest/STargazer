2df1e98eb5d272a9333b0d01afd4a4b26d6ada56
Curve <fynnbwdt@gmail.com>
2023-08-10 18:45:24 +0200

    fix(tests): wikipedia test

--- tests/wikipedia.test.ts
+++ tests/wikipedia.test.ts
@@ -1,41 +1,47 @@
-import { readFileSync } from 'fs';
-import { JSDOM } from 'jsdom';
-import path from 'path';
-import { defaultSettings } from '../src/settings/defaults';
-import generateSelectors from '../src/generator';
-import setup, { setInspected } from './setup';
+import { readFileSync } from "fs";
+import { JSDOM } from "jsdom";
+import path from "path";
+import { generator } from "../src/generator";
+import { set_inspected, setup } from "./setup";
 
-const html = readFileSync(path.join(__dirname, 'wikipedia.html')).toString();
-const fakeDom = new JSDOM(html);
+const html = readFileSync(path.join(__dirname, "wikipedia.html")).toString();
+const { window } = new JSDOM(html);
 
-describe('Wikipedia', () => {
-  beforeAll(() => {
-    setup(fakeDom.window.document);
-  });
+describe("Wikipedia", () =>
+{
+    beforeAll(() =>
+    {
+        setup(window.document);
+    });
 
-  it('Selectors for Search-Input', async () => {
-    setInspected(fakeDom.window.document.querySelector('#txtSearch'));
-    const selectors = await generateSelectors(defaultSettings);
+    it("Selectors for Search-Input", async () =>
+    {
+        set_inspected(window.document.querySelector("#txtSearch"));
 
-    expect(selectors.length).toBeLessThanOrEqual(defaultSettings.resultsToDisplay);
+        const selectors = await generator.generate();
+        const top = selectors.slice(0, 3);
 
-    expect(selectors.at(0).selector).toBe('#txtSearch');
-    expect(selectors.at(1).selector).toBe('[name="q"]');
-  });
+        expect(top.findIndex(x => x.selector === "#txtSearch")).toBeGreaterThan(-1);
+        expect(top.findIndex(x => x.selector === "[name=\"q\"]")).toBeGreaterThan(-1);
+    });
 
-  it('Selectors for Search-Icon', async () => {
-    setInspected(fakeDom.window.document.querySelector('.search-icon'));
-    const selectors = await generateSelectors(defaultSettings);
+    it("Selectors for Search-Icon", async () =>
+    {
+        set_inspected(window.document.querySelector(".search-icon"));
 
-    expect(selectors.length).toBeLessThanOrEqual(defaultSettings.resultsToDisplay);
-    expect(selectors.at(0).selector).toBe('.search-icon');
-  });
+        const selectors = await generator.generate();
+        const top = selectors.slice(0, 3);
 
-  it('Selectors for Logo', async () => {
-    setInspected(fakeDom.window.document.querySelector('.wikipedia-logo a img'));
-    const selectors = await generateSelectors(defaultSettings);
+        expect(top.findIndex(x => x.selector === ".search-icon")).toBeGreaterThan(-1);
+    });
 
-    expect(selectors.length).toBeLessThanOrEqual(defaultSettings.resultsToDisplay);
-    expect(selectors.at(0).selector).toBe('.wikipedia-logo a img');
-  });
+    it("Selectors for Logo", async () =>
+    {
+        set_inspected(window.document.querySelector(".wikipedia-logo a img"));
+
+        const selectors = await generator.generate();
+        const top = selectors.slice(0, 5);
+
+        expect(top.findIndex(x => x.selector === ".wikipedia-logo :nth-child(1) :nth-child(1)")).toBeGreaterThan(-1);
+    });
 });
